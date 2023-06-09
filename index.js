c4f69ea9f245d1887d50080f37b8cd7895726862
Raúl Low <54368711+raulow92@users.noreply.github.com>
2023-06-09 11:07:09 -0400

    Adding process.env.PORT, utf-8 and comments

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const routes = require("./routes")
 const app = express();
-const port = 3000
+const PORT = process.env.PORT || 3000
 
-app.listen(port, console.log(`Servidor activo en el puerto ${3000}` ));
+app.listen(PORT, console.log(`Servidor activo en el puerto ${3000}`));
+
+//Middlewares
 app.use(express.json());
 
 app.use('/', routes)
--- routes.js
+++ routes.js
@@ -1,43 +1,46 @@
-const express = require("express")
-const fs = require("fs");
+const express = require('express')
+const fs = require('fs');
 const router = express.Router()
 
-router.get("/canciones", (req, res) => {
-    const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+// Consultar canciones
+router.get('/canciones', (req, res) => {
+    const canciones = JSON.parse(fs.readFileSync('repertorio.json','utf-8'));
     res.json(canciones);
 });
 
-router.post("/canciones", (req, res) => {
+// Crear canciones
+router.post('/canciones', (req, res) => {
     const cancion = req.body;
-    const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+    const canciones = JSON.parse(fs.readFileSync('repertorio.json', 'utf-8'));
     canciones.push(cancion);
-
-    fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
-
-    res.send("Canción agregada con éxito!");
+    fs.writeFileSync('repertorio.json', JSON.stringify(canciones));
+    res.send('Canción agregada con éxito!');
 });
 
-router.delete("/canciones/:id", (req, res) => {
+// Eliminar canciones
+router.delete('/canciones/:id', (req, res) => {
     const { id } = req.params;
-    const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+    const canciones = JSON.parse(fs.readFileSync('repertorio.json', 'utf-8'));
     const index = canciones.findIndex((p) => p.id == id);
     canciones.splice(index, 1);
-    fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
-    res.send("Canción eliminada con éxito");
+    fs.writeFileSync('repertorio.json', JSON.stringify(canciones));
+    res.send('Canción eliminada con éxito');
 });
 
-router.put("/canciones/:id", (req, res) => {
+// Editar canciones
+router.put('/canciones/:id', (req, res) => {
     const { id } = req.params;
     const cancion = req.body;
-    const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+    const canciones = JSON.parse(fs.readFileSync('repertorio.json', 'utf-8'));
     const index = canciones.findIndex((p) => p.id == id);
     canciones[index] = cancion;
-    fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
-    res.send("Canción modificada con éxito");
+    fs.writeFileSync('repertorio.json', JSON.stringify(canciones));
+    res.send('Canción modificada con éxito');
 });
 
-router.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+// Enlace página principal
+router.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
 module.exports = router
